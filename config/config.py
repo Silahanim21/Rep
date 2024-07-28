910e3e478db47e59609503aa60da4f2c3dad8fa0
Madboy0700 <bobimobi07@gmail.com>
2024-07-28 21:22:42 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -18,7 +18,7 @@ API_HASH = getenv("API_HASH", "eaa33aa1d9c47b197c3c81a8da988378")
 
 ## Get it from @Botfather in Telegram.
 
-BOT_TOKEN = getenv("BOT_TOKEN", "6757500151:AAFm19zqmp-RBe4W6-XyIKv11wxxGBaCpdw")
+BOT_TOKEN = getenv("BOT_TOKEN", "6757500151:AAGUL0YoLZJYKR93P-xpb_RjpqhLJTOQ3kw")
 # Database to save your chats and stats.
 
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://erkbwrs084:909090@cluster0.qdrfgmb.mongodb.net/?retryWrites=true&w=majority")
